0397c64a568e3b67c837ea511efd3678364e8c95
David Lee <49456773+lee5378@users.noreply.github.com>
2020-12-25 21:08:34 -0800

    Update labsetup.sh

--- labsetup.sh
+++ labsetup.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-```
-Homelab deployment automation script
-```
+# Homelab deployment automation script
+
+cd ~/Downloads
+touch deployerlog.txt
 
 sudo apt update
 sudo apt-get update
@@ -10,37 +11,43 @@ sudo apt upgrade
 
 # Install VIM
 sudo apt install vim -y
-vim --version
+vim --version >> ~/Downloads/deployerlog.txt
+
+# Install curl
+sudo apt install curl -y
+curl --version >> ~/Downloads/deployerlog.txt
 
 # Install Virtalbox using latest .deb file
-cd ~/Downloads
-curl -O https://download.virtualbox.org/virtualbox/6.1.16/virtualbox-6.1_6.1.16-140961~Ubuntu~eoan_amd64.deb
-sudo dpkg -i virtualbox-6.1_6.1.16-140961~Ubuntu~eoan_amd64.deb
-virtualbox --version
+wget -q https://www.virtualbox.org/download/oracle_vbox_2016.asc -O- | sudo apt-key add -
+wget -q https://www.virtualbox.org/download/oracle_vbox.asc -O- | sudo apt-key add -
+echo "deb [arch=amd64] http://download.virtualbox.org/virtualbox/debian eoan contrib" | sudo tee /etc/apt/sources.list.d/virtualbox.list
 
 # Install Virtualbox Extension Pack
-curl -O https://download.virtualbox.org/virtualbox/6.1.16/Oracle_VM_VirtualBox_Extension_Pack-6.1.16.vbox-extpack
-sudo VBoxManage extpack install Oracle_VM_VirtualBox_Extension_Pack-6.1.16.vbox-extpack
+sudo apt update
+sudo apt install linux-headers-$(uname -r) dkms
+sudo apt-get install virtualbox-6.1 -y
+wget https://download.virtualbox.org/virtualbox/6.1.6/Oracle_VM_VirtualBox_Extension_Pack-6.1.6.vbox-extpack
+vboxmanage extpack install Oracle_VM_VirtualBox_Extension_Pack-6.1.6.vbox-extpack
+virtualbox --version >> ~/Downloads/deployerlog.txt
 
 # Install GNS3
 sudo add-apt-repository ppa:gns3/ppa
 sudo apt install gns3-server gns3-gui -y
 echo "Press Yes for both the next prompts"
-gns3 --version
+gns3 --version >> ~/Downloads/deployerlog.txt
 sudo usermod -aG kvm $(whoami)
 sudo apt install xtightvncviewer -y
 
 # Install Git
 sudo apt install git -y
-git --version
+git --version >> ~/Downloads/deployerlog.txt
 
 # Install VS Code
 sudo apt install software-properties-common apt-transport-https wget -y
 wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main"
 sudo apt install code -y
-code --version
+code --version >> ~/Downloads/deployerlog.txt
 
-# Remove installation files
-rm virtualbox-6.1_6.1.16-140961~Ubuntu~eoan_amd64.deb
-rm Oracle_VM_VirtualBox_Extension_Pack-6.1.16.vbox-extpack
+echo "Deployment script completed. Here's what is now installed:"
+cat ~/Downloads/deployerlog.txt
