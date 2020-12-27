ea3c2cf587ee0d1282d1c03de7a13ecc16c70a97
David Lee <lee5378@alumni.uidaho.edu>
2020-12-27 00:56:02 -0500

    revisions

--- deployelk.sh
+++ deployelk.sh
@@ -1,6 +1,7 @@
 # Deploy DetectionLabELK
 
 # DetectionLabELK is a premade set of VMs that allow us to skip the deployment of various security systems and jump right into a domained network and start simulating threats.
+# This installation process takes about 5 hours. Keep the executing terminal open and a Virtualbox Manager GUI open to track installation progress.
 
 # Install Vagrant, a DetectionLabELK deployment dependency
 curl -O https://releases.hashicorp.com/vagrant/2.2.14/vagrant_2.2.14_x86_64.deb
@@ -20,5 +21,5 @@ vagrant plugin install vagrant-reload
 mkdir ~/github
 cd ~/github
 git clone https://github.com/cyberdefenders/DetectionLabELK.git
-cd ~/github/DetectionLabELK/Vagrant
-vagrant up
+cd ~/github/DetectionLabELK
+./build.sh virtualbox
