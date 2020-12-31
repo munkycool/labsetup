8a43e545564217d1a0afbe6ee7d444f81f5088fc
David Lee <lee5378@alumni.uidaho.edu>
2020-12-31 02:04:32 -0500

    bind9 script revisions

--- install-bind.sh
+++ install-bind.sh
@@ -1,3 +1,18 @@
+#!/bin/bash
+
+domain = "mydomain"
+ipdns = "192.168.0.40"
+ipnet = "192.168.0.0/24"
+
+echo "Please name your local domain (e.g. for homelab.local just type homelab):"
+read domain
+
+echo "What is the IP address of this DNS server?"
+read ipdns
+
+echo "What is the network address including CIDR block (e.g 192.168.0.0/24)?"
+read ipnet
+
 sudo apt-get update
 sudo apt-get install bind9 bind9utils bind9-doc dnsutils -y
 
@@ -11,8 +26,8 @@ options {
         directory "/var/cache/bind";
         auth-nxdomain no;    # conform to RFC1035
      // listen-on-v6 { any; };
-        listen-on port 53 { localhost; 192.168.1.0/24; };
-        allow-query { localhost; 192.168.1.0/24; };
+        listen-on port 53 { localhost; $ipnet; };
+        allow-query { localhost; $ipnet; };
         forwarders { 8.8.8.8; };
         recursion yes;
         };
@@ -20,23 +35,23 @@ EOT
 
 # Update named.conf.local
 cat <<EOT >> /etc/bind/named.conf.local
-zone    "alphacyberlabs.local"   {
+zone    "$domain.local"   {
         type master;
-        file    "/etc/bind/forward.alphacyberlabs.local";
+        file    "/etc/bind/forward.$domain.local";
  };
 
 zone   "1.168.192.in-addr.arpa"        {
        type master;
-       file    "/etc/bind/reverse.alphacyberlabs.local";
+       file    "/etc/bind/reverse.$domain.local";
  };
 EOT
 
 # Create forward lookup zone file
-sudo touch /etc/bind/forward.alphacyberlabs.local
-cat <<EOT >> /etc/bind/forward.alphacyberlabs.local
-$TTL    604800
+sudo touch /etc/bind/forward.$domain.local
+cat <<EOT >> /etc/bind/forward.$domain.local
+\$TTL    604800
 
-@       IN      SOA     primary.alphacyberlabs.local. root.primary.alphacyberlabs.local. (
+@       IN      SOA     primary.$domain.local. root.primary.$domain.local. (
                               6         ; Serial
                          604820         ; Refresh
                           86600         ; Retry
@@ -44,27 +59,27 @@ $TTL    604800
                          604600 )       ; Negative Cache TTL
 
 ;Name Server Information
-@       IN      NS      primary.alphacyberlabs.local.
+@       IN      NS      primary.$domain.local.
 
 ;IP address of Your Domain Name Server(DNS)
-primary IN       A      192.168.1.5
+primary IN       A      $ipdns
 
 ;Mail Server MX (Mail exchanger) Record
-alphacyberlabs.local. IN  MX  10  mail.alphacyberlabs.local.
+$domain.local. IN  MX  10  mail.$domain.local.
 
 ;A Record for Host names
 www     IN       A       192.168.1.50
 mail    IN       A       192.168.1.60
 
 ;CNAME Record
-ftp     IN      CNAME    www.alphacyberlabs.local.
+ftp     IN      CNAME    www.$domain.local.
 EOT
 
 # Create reverse lookup zone file
-sudo touch /etc/bind/reverse.alphacyberlabs.local
-cat <<EOT >> /etc/bind/reverse.alphacyberlabs.local
-$TTL    604800
-@       IN      SOA     alphacyberlabs.local. root.alphacyberlabs.local. (
+sudo touch /etc/bind/reverse.$domain.local
+cat <<EOT >> /etc/bind/reverse.$domain.local
+\$TTL    604800
+@       IN      SOA     $domain.local. root.$domain.local. (
                              21         ; Serial
                          604820         ; Refresh
                           864500        ; Retry
@@ -72,15 +87,15 @@ $TTL    604800
                          604880 )       ; Negative Cache TTL
 
 ;Your Name Server Info
-@       IN      NS      primary.alphacyberlabs.local.
-primary IN      A       192.168.1.40
+@       IN      NS      primary.$domain.local.
+primary IN      A       $ipdns
 
 ;Reverse Lookup for Your DNS Server
-40      IN      PTR     primary.alphacyberlabs.local.
+40      IN      PTR     primary.$domain.local.
 
 ;PTR Record IP address to HostName
-50      IN      PTR     www.alphacyberlabs.local.
-60      IN      PTR     mail.alphacyberlabs.local.
+50      IN      PTR     www.$domain.local.
+60      IN      PTR     mail.$domain.local.
 EOT
 
 # Apply changes
